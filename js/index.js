b327886ae0d933955ec7310a1dc4e5329ee20527
WilFer-10 <evolution.king@gmail.com>
2023-04-05 14:00:03 +0700

    Fix Input Form & Insert Formula - No Result - Problem Output

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 // Calculate Triangle Area & Perimeter
-
-
-const a = parseFloat(document.getElementsById("tr.base.a").value)
-const t = parseFloat(document.getElementsById("tr.height").value)
-
-function triarea() {
-    const L = (a * t) / 2
-}
+function triarea(){
+    var a = document.getElementById("tr.base").value;
+    var t = document.getElementById("tr.height").value;
+    var area = (a * t) / 2;
+    var result = "Area: " + area ;
+    document.getElementById("tr.area").innerHTML = result; 
+  }
