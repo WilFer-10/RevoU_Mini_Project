b38b5b4a8cd0edfc4f38a99f20ed22994c9859d5
WilFer-10 <evolution.king@gmail.com>
2023-04-05 22:53:59 +0700

    Formula For Triangle Area, Fix Output HTML

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 // Calculate Triangle Area & Perimeter
-function triarea(){
-    var a = document.getElementById("tr.base").value;
-    var t = document.getElementById("tr.height").value;
-    var area = (a * t) / 2;
-    var result = "Area: " + area ;
-    document.getElementById("tr.area").innerHTML = result; 
+
+function triarea() {
+    const a = parseFloat(document.getElementById("tri.base").value);
+    const t = parseFloat(document.getElementById("tri.height").value);
+    let tri_area = (a * t) / 2;
+    //const result_cal = "L = ½ x " + a + " x " + t;
+    const result = "L = " + tri_area;
+    document.getElementById("area.for").textContent = "L = ½ x a x t";
+    document.getElementById("area.cal").textContent = "L = ½ x " + a + " x " + t;
+    document.getElementById("tri.area").textContent = result; 
   }
