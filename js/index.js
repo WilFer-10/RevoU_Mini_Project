02682989c8bf09643512030441feb2eb0502db28
WilFer-10 <evolution.king@gmail.com>
2023-04-05 09:58:31 +0700

    Fix Form Style & Buttons

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Calculate Triangle Area & Perimeter
+
+
+const a = parseFloat(document.getElementsById("tr.base.a").value)
+const t = parseFloat(document.getElementsById("tr.height").value)
+
+function triarea() {
+    const L = (a * t) / 2
+}
