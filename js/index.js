1d294d75b8f71716cb265bcbb67f545c082a05c1
WilFer-10 <evolution.king@gmail.com>
2023-04-06 08:30:55 +0700

    Style Result Text & Update Result in JS

--- js/index.js
+++ js/index.js
@@ -4,9 +4,7 @@ function triarea() {
     const a = parseFloat(document.getElementById("tri.base").value);
     const t = parseFloat(document.getElementById("tri.height").value);
     let tri_area = (a * t) / 2;
-    //const result_cal = "L = ½ x " + a + " x " + t;
-    const result = "L = " + tri_area;
     document.getElementById("area.for").textContent = "L = ½ x a x t";
-    document.getElementById("area.cal").textContent = "L = ½ x " + a + " x " + t;
-    document.getElementById("tri.area").textContent = result; 
+    document.getElementById("area.cal").textContent = "L = ½ x " + a + " cm x " + t + " cm = " + tri_area + " cm";
+    document.getElementById("tri.area").textContent = "The area of triangle is " + tri_area + " cm"; 
   }
